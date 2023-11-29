3db83dd3bae0b5c8fef8f8aca03602308af58ab2
GongXian Cao <gongxiancao@hotmail.com>
2023-11-29 09:05:44 +0800

    Refactor population run

--- lib/src/population.dart
+++ lib/src/population.dart
@@ -74,62 +74,22 @@ class Population {
 
       // Evaluate all genomes using the user-provided function.
       fitnessDelegate.evaluate(genomes: population.values, config: config);
-
       // Gather and report statistics.
-      Genome? best;
-      for (final g in population.values) {
-        if (g.fitness == null) {
-          throw RuntimeException('Fitness not assigned to genome ${g.key}');
-        }
+      Genome? best = getBestGenome();
 
-        if (best == null || g.fitness! > best.fitness!) {
-          best = g;
-        }
-      }
       reporter.postEvaluate(config: config, population: population, speciesSet: species, bestGenome: best!);
 
       // Track the best genome ever seen.
       if (bestGenome == null || best.fitness! > bestGenome!.fitness!) {
         bestGenome = best;
       }
-
-      if (!config.noFitnessTermination) {
-        // End if the fitness threshold is reached.
-        final fitnesses = <double>[];
-        for (final g in population.values) {
-          fitnesses.add(g.fitness!);
-        }
-
-        final fv = config.fitnessCriterion(fitnesses);
-        if (config.fitnessThreshold != 0 && fv >= config.fitnessThreshold) {
-          reporter.foundSolution(config: config, generation: generation, bestGenome: best);
-          break;
-        }
-      }
-
-      // Create the next generation from the current generation.
-      population = reproduction.reproduce(config: config, speciesSet: species,
-        popSize: config.popSize, generation: generation);
-
-      // Check for complete extinction.
-      if (species.species.isEmpty) {
-        reporter.completeExtinction();
-
-        // If requested by the user, create a completely new population,
-        // otherwise raise an exception.
-        if (config.resetOnExtinction) {
-          population = reproduction.createNew(config.genome, config.popSize);
-        } else {
-          throw CompleteExtinctionException();
-        }
-      }
-
-      // Divide the new population into species.
-      species.speciate(config: config, population: population, generation: generation);
-
       reporter.endGeneration(config: config, generation: generation, population: population, speciesSet: species);
 
-      generation += 1;
+      if (checkFitnessThreshold()) {
+        break;
+      }
+
+      nextGeneration();
     }
 
     if (config.noFitnessTermination) {
@@ -137,5 +97,75 @@ class Population {
     }
 
     return bestGenome!;
+  }
+
+  Genome? getBestGenome() {
+    // Gather and report statistics.
+    Genome? best;
+    for (final g in population.values) {
+      if (g.fitness == null) {
+        throw RuntimeException('Fitness not assigned to genome ${g.key}');
+      }
+
+      if (best == null || g.fitness! > best.fitness!) {
+        best = g;
+      }
+    }
+    return best;
+  }
+
+  bool checkFitnessThreshold() {
+    // End if the fitness threshold is reached.
+    final fitnesses = <double>[];
+    for (final g in population.values) {
+      fitnesses.add(g.fitness!);
+    }
+
+    final fv = config.fitnessCriterion(fitnesses);
+    if (config.fitnessThreshold != 0 && fv >= config.fitnessThreshold) {
+      return true;
+    }
+    return false;
+  }
+
+  void nextGeneration() {
+    // Gather and report statistics.
+    ++generation;
+    Genome? best;
+    for (final g in population.values) {
+      if (g.fitness == null) {
+        throw RuntimeException('Fitness not assigned to genome ${g.key}');
+      }
+
+      if (best == null || g.fitness! > best.fitness!) {
+        best = g;
+      }
+    }
+
+    // Track the best genome ever seen.
+    if (bestGenome == null || best!.fitness! > bestGenome!.fitness!) {
+      bestGenome = best;
+    }
+
+    // Create the next generation from the current generation.
+    population = reproduction.reproduce(config: config, speciesSet: species,
+        popSize: config.popSize, generation: generation);
+
+    // Check for complete extinction.
+    if (species.species.isEmpty) {
+      reporter.completeExtinction();
+
+      // If requested by the user, create a completely new population,
+      // otherwise raise an exception.
+      if (config.resetOnExtinction) {
+        population = reproduction.createNew(config.genome, config.popSize);
+      } else {
+        throw CompleteExtinctionException();
+      }
+    }
+
+    // Divide the new population into species.
+    species.speciate(
+        config: config, population: population, generation: generation);
   }
 }
